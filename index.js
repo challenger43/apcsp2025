f122b5c37ce845260b7af6946420759dc72f457f
challenger43 <isabella.lo28@student.tamdistrict.org>
2025-11-19 18:37:25 -0800

    Reworked some of the logic, swapped workout intensity to radio buttons, added body section logic

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-function changeValue(){
-    console.log("hi")
+const bodySectionCheckboxes = document.getElementsByName("bodySections")
+let bodySectionCheckedCount = 0;
+let selectedBodySections = [];
+const maxBodySectionCheckables = 3;
+
+for (const bodySectionCheckbox of bodySectionCheckboxes) {
+    bodySectionCheckbox.addEventListener("change", function () {
+        if (this.checked) {
+            if (selectedBodySections.length >= maxBodySectionCheckables) {
+                this.checked = false;
+                alert(`You can only select up to ${maxBodySectionCheckables} options.`);
+                return;
+            }
+            selectedBodySections.push(this.value); 
+        } else {
+            selectedBodySections = selectedBodySections.filter(value => value !== this.value); // Remove from array if unchecked
+        }
+        console.log(selectedBodySections);
+    });
 }
