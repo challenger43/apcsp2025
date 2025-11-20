d9e6358273c998510904e11e867919342d8dfe3f
challenger43 <isabella.lo28@student.tamdistrict.org>
2025-11-20 13:35:39 -0800

    Basic prompting system finished, ready to move onto official 'algorithm'

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const bodySectionCheckboxes = document.getElementsByName("bodySections")
 let bodySectionCheckedCount = 0;
 let selectedBodySections = [];
 const maxBodySectionCheckables = 3;
+const intensityLevel = document.getElementsByName("workoutIntensity")
+let selectedIntensityValue;
 
-for (const bodySectionCheckbox of bodySectionCheckboxes) {
+for (const bodySectionCheckbox of bodySectionCheckboxes) { //finds what values are selected from the checkbox and puts it into an array for future usage 
     bodySectionCheckbox.addEventListener("change", function () {
         if (this.checked) {
             if (selectedBodySections.length >= maxBodySectionCheckables) {
@@ -11,10 +13,30 @@ for (const bodySectionCheckbox of bodySectionCheckboxes) {
                 alert(`You can only select up to ${maxBodySectionCheckables} options.`);
                 return;
             }
-            selectedBodySections.push(this.value); 
+            selectedBodySections.push(this.value);
         } else {
-            selectedBodySections = selectedBodySections.filter(value => value !== this.value); // Remove from array if unchecked
+            selectedBodySections = selectedBodySections.filter(value => value !== this.value); // Remove from array if unchecked, for each value if the value doesn't equal the value that is checked/ want to remove
+            //it will evaluate to true and those that are true and put it into a new array, which will then be assigned to the same selectedBodySections array. 
         }
         console.log(selectedBodySections);
     });
+}
+function getValues() {
+    const selectedIntensity = document.querySelector('input[name="workoutIntensity"]:checked')
+    if (selectedIntensity){
+        selectedIntensityValue = selectedIntensity.value;
+    }
+    if (selectedIntensity && !selectedBodySections.length == 0) {
+        return true
+    }
+    else {
+        alert("Please fill out the whole form");
+        return false
+    }
+}
+function generateWorkout() {
+    if (!getValues()){
+        return
+    }
+    console.log(`Successful Execution of generateWorkout ${selectedBodySections}, ${selectedIntensityValue}`)
 }
