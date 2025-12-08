26cfa0895efc6089a6a8417a062bf201b94ea6b1
challenger43 <isabella.lo28@student.tamdistrict.org>
2025-12-08 11:09:36 -0800

    finished the actual program, now need to brainstorm how to add a parameter

--- index.js
+++ index.js
@@ -3,20 +3,17 @@ const legWorkouts = ["bodyweight squats", "lunge/reverse lunges", "calf raises",
 const abWorkouts = ["situps", "curl up/crunches", "oblique heel touches", "mountain climbers", "Russian Twist"]
 const cardioEnduranceWorkouts = ["high knees", "jog(in place)", "upper arm plank", "forearm plank", "squat hold"]
 const flexibilityWorkouts = ["hamstring stretch", "downward dog", "quad stretch", "butterfly stretch", "hip stretch"]
-//each array has 3 levels of difficulty
-//arms, legs, abs, cardio, flexibility
 const bodySectionCheckboxes = document.getElementsByName("bodySections")
 let bodySectionCheckedCount = 0;
 let selectedBodySections = [];
 const maxBodySectionCheckables = 3;
 const intensityLevel = document.getElementsByName("workoutIntensity")
+const para1 = document.getElementById("para1")
 let selectedIntensityValue;
 let numReps;
 let numExercises;
 let finalWorkout;
-const para1 = document.getElementById("para1")
-const para2 = document.getElementById("para2")
-const para3 = document.getElementById("para3")
+let workoutType;
 
 for (const bodySectionCheckbox of bodySectionCheckboxes) { //finds what values are selected from the checkbox and puts it into an array for future usage 
     bodySectionCheckbox.addEventListener("change", function () {
@@ -51,6 +48,7 @@ function generateWorkout() {
     if (!getValues()) {
         return
     }
+    para1.innerHTML = ""
     switch (selectedIntensityValue) {
         case "low":
             numReps = 5
@@ -68,47 +66,44 @@ function generateWorkout() {
     let tempArray = []
     let tempValue;
     let feederWorkout
-    let numGenerated = 0
-    let paraNum;
     for (let section of selectedBodySections) {
-        numGenerated += 1
         switch (section) {
             case "upperBody":
                 feederWorkout = upperBodyWorkouts
+                workoutType = "reps"
                 break
             case "legs":
                 feederWorkout = legWorkouts
+                workoutType = "reps"
                 break
             case "abs":
                 feederWorkout = abWorkouts
+                workoutType = "reps"
                 break
             case "endurance":
                 feederWorkout = cardioEnduranceWorkouts
+                workoutType = "time"
                 break
             case "flexibility":
                 feederWorkout = flexibilityWorkouts
+                workoutType = "time"
                 break
         }
         let lenArray = feederWorkout.length
-        for (let i = 0; i < lenArray; i++){
-            tempValue = feederWorkout[Math.floor(Math.random()*feederWorkout.length)]
+        for (let i = 0; i < lenArray; i++) {
+            tempValue = feederWorkout[Math.floor(Math.random() * feederWorkout.length)]
             tempArray.push(tempValue)
             feederWorkout = feederWorkout.filter(value => value !== tempValue)
         }
-        for (let i = 0; i < numExercises; i++){
+        for (let i = 0; i < numExercises; i++) {
             feederWorkout.push(tempArray.pop())
             console.log(feederWorkout)
         }
-        switch (numGenerated){
-            case "1":
-                paraNum = para1
-                break
-            case "2":
-                paraNum = para2
-                break
-            case "3":
-                paraNum = para3
-                break
+        if (workoutType === "reps") {
+            para1.innerHTML += `<h1>Body Section: ${section},</h1> <h2>Exercises:</h2> <p>Reps: ${numReps}</p> <p>${feederWorkout}</p>`
+        }
+        else {
+            para1.innerHTML += `<h1>Body Section: ${section},</h1> <h2>Exercises:</h2> <p>Time: ${numReps} seconds</p> <p>${feederWorkout}</p>`;
         }
     }
 }
